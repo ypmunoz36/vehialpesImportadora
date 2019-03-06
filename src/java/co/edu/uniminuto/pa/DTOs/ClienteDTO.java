455df99ccf81a341f9b6bd3cb09f9141b3630ddf
sala8 <sala8@is-ls08e06.FI-LabInf.ucatolica.loc>
2019-03-06 18:48:02 -0500

    Clase cliente

---
+++ src/java/co/edu/uniminuto/pa/DTOs/ClienteDTO.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniminuto.pa.DTOs;
+
+/**
+ *
+ * @author sala8
+ */
+public class ClienteDTO {
+     
+    private int identificacion =0;
+    private String nombre  = null;
+    private String apellidos = null ;
+    private String direccion = null;
+    private int ciudad = 0;
+    private String telefono = null;
+    private String celular = null;
+    private int tipo = 0 ;
+    private String fechaNacimiento = null;
+    private String correo = null;
+    private String sexo = null;
+    private String descripcion = null;
+
+    public ClienteDTO() {
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(int ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "ClienteDTO{" + "identificacion=" + identificacion + ", nombre=" + nombre + ", apellidos=" + apellidos + ", direccion=" + direccion + ", ciudad=" + ciudad + ", telefono=" + telefono + ", celular=" + celular + ", tipo=" + tipo + ", fechaNacimiento=" + fechaNacimiento + ", correo=" + correo + ", sexo=" + sexo + ", descripcion=" + descripcion + '}';
+    }
+    
+    
+    
+}
